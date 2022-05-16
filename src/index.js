a652bcaece9f5462b9a7597a22c38f08f841d842
alexchamorro0x <oriondigitalconsulting@gmail.com>
2022-05-16 17:24:00 +0200

    Add search bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.scss';
+// import addElem from './modules/add-elem.js';
+
+// Search button
+const searchIcon = document.querySelector('#search-btn');
+const searchBarContainer = document.querySelector('.search-bar');
+const searchCloseBtn = document.querySelector('#search-close-btn');
+
+searchIcon.onclick = () => {
+  searchBarContainer.classList.remove('hide');
+
+  // Add event listener
+  searchCloseBtn.onclick = () => {
+    searchBarContainer.classList.add('hide');
+  };
+};
