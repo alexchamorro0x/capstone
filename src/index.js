9cd5d2219a982e72c05ccef421ef8659e123be4e
alexchamorro0x <oriondigitalconsulting@gmail.com>
2022-05-18 00:34:23 +0200

    Add comments section (with API) and close popup on click outside

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.scss';
 import getData from './modules/tvmaze.js';
 import { getLikes, postLike } from './modules/involvement.js';
-import { showPopup } from './modules/popup.js';
+import { showPopup, clearPopups } from './modules/popup.js';
 // import addElem from './modules/add-elem.js';
 
 // Search button
@@ -232,8 +232,10 @@ const createElementForShows = async (requestURL) => {
         // Pop-up trigger event
         const showData = el;
         div.addEventListener('click', (e) => {
-          // console.log(showData);
-          showPopup(showData, e.target.getBoundingClientRect());
+          if (!e.target.matches('.starBorder')) {
+            // console.log(e.target);
+            showPopup(showData, e.target.closest('.cardItem').getBoundingClientRect());
+          }
         });
       });
     });
@@ -249,4 +251,12 @@ window.onload = () => {
 const h1 = document.querySelector('h1');
 h1.addEventListener('click', () => {
   window.location.reload();
+});
+
+// Event listener on the document
+// If the click is not on the cardItem and not on the popup-container, clear the popups
+document.addEventListener('click', (e) => {
+  if (!e.target.closest('.cardItem') && !e.target.closest('.popup-container')) {
+    clearPopups();
+  }
 });
--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,4 +1,5 @@
 import addElem from './add-elem.js';
+import { postComment, getComments } from './involvement.js';
 
 const clearPopups = () => {
   const popupContainer = document.querySelectorAll('.popup-container');
@@ -10,10 +11,52 @@ const clearPopups = () => {
   }
 };
 
+const updateComments = async (_id, _container) => {
+  let comments = await getComments(_id);
+  comments = Array.isArray(comments) ? comments : [];
+
+  // Add comments section to the container
+  _container.innerHTML = `
+    <div class="comments-current">
+      <h3>Comments (${comments.length})</h3>
+      <div class="comments-all"></div>
+    </div>
+    <div class="comments-add">
+      <h3>Add a comment</h3>
+      <form class="form-add-comment" action="">
+        <input class="input-comment-name" type="text" placeholder="Your name">
+        <input class="input-comment-insight" type="text" placeholder="Your insights">
+        <button type="submit">Comment</button>
+      </form>
+    </div>`;
+
+  // Generate current comments
+  const commentsAll = document.querySelector('.comments-all');
+  if (comments) {
+    comments.forEach((comment) => {
+      commentsAll.innerHTML += `
+        <span>${comment.creation_date}</span>
+        <span>${comment.username}:</span>
+        <span>${comment.comment}</span>`;
+    });
+  }
+
+  // Add form event listener
+  const form = document.querySelector('.form-add-comment');
+  const inputName = form.querySelector('.input-comment-name');
+  const inputInsight = form.querySelector('.input-comment-insight');
+
+  form.onsubmit = (e) => {
+    e.preventDefault();
+    postComment(_id, inputName.value, inputInsight.value);
+
+    form.reset();
+  };
+};
+
 const showPopup = (_showData, _domRect) => {
   // Clear all other pop-ups if any
   clearPopups();
-
   // Calculate y position
   const posY = window.pageYOffset + _domRect.y - 50;
 
@@ -40,13 +83,18 @@ const showPopup = (_showData, _domRect) => {
     </div>
     <img class="popup-img" src="${_showData.image.original}" alt="show thumbnail">
     <div class="genres flex-row"></div>
-    <div>${_showData.summary}</div>`;
+    <div>${_showData.summary}</div>
+    <div class="comments-container"></div>`;
 
   // Generate genres
   const genres = document.querySelector('.genres');
   _showData.genres.forEach((genre) => {
     genres.innerHTML += `<div class="tag-genre">${genre}</div>`;
   });
+
+  // Generate comments
+  const commentsContainer = document.querySelector('.comments-container');
+  updateComments(_showData.id, commentsContainer);
 
   // Close button event listener
   const popupCloseContainer = document.querySelector('.popup-close-container');
